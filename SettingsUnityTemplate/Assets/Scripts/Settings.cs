74047f9de1719fff2d6a5e5fce358ffec28dcd51
Dean-Overton <64393357+Dean-Overton@users.noreply.github.com>
2020-07-05 14:58:07 +1000

    Here is a beginner settings UI template...
    Contains audio manager for sound FX and has mute button in settings panel :)

---
+++ SettingsUnityTemplate/Assets/Scripts/Settings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer audioMixer;
+    [SerializeField]
+    private ToggleController musicToggle;
+    public bool musicOn;
+    [SerializeField]
+    private ToggleController soundToggle;
+    public bool soundOn;
+    public void ToggleSound ()
+    {
+        soundOn = soundToggle.isOn;
+        if (soundOn)
+            audioMixer.SetFloat("SoundVol", 0f);
+        else
+            audioMixer.SetFloat("SoundVol", -80f);
+    }
+    public void ToggleMusic()
+    {
+        musicOn = musicToggle.isOn;
+        if (musicOn)
+            audioMixer.SetFloat("MusicVol", 0f);
+        else
+            audioMixer.SetFloat("MusicVol", -80f);
+    }
+}
